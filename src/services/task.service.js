752d38deb1785abf3482f8970319bcb45dba2568
ChrisCore <222845016+ChrisCore1@users.noreply.github.com>
2026-08-23 16:24:33 -0400

    TOD-5: Implementar función getAll() en tarea.service.js (#3)
    Co-authored-by: ChrisCore1 <ChrisCore1@users.noreply.github.com>

---
+++ src/services/task.service.js
@@ -0,0 +1,24 @@
+const API_URL = 'http://localhost:8000/api';
+
+export const getAll = async () => {
+  try{
+    const response = await fetch(`${API_URL}/tasks`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+    });
+
+    if(!response.ok){
+      throw new Error(`Error con la peticion: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data;
+
+  }catch (e){
+    console.error('Error al obtener las tareas: ', e);
+    throw e;
+  }
+};
