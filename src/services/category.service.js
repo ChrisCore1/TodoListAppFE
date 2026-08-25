4a178bd147ac8f94bfd1bb56642432ba995e8954
ChrisCore1 <ChrisCore1@users.noreply.github.com>
2026-08-24 23:32:44 -0400

    TOD-7: Cambios implementados

--- src/services/category.service.js
+++ src/services/category.service.js
@@ -1,18 +1,4 @@
-const API_URL = 'http://localhost:8000/api';
-
-const HEADERS = {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
-};
-
-const handleResponse = async (response) => {
-    if(!response.ok){
-        const errorData = await response.json().catch(() => ({}));
-        console.error(errorData);
-        throw new Error(`Error en la peticion: ${response.statusText}`);
-    }
-    return response.json();
-};
+import { API_URL, HEADERS, handleResponse } from './service'
 
 export const getAll = async () => {
     const response = await fetch(`${API_URL}/categories`, {
