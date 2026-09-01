621ea31afc0b96033423be3ca407a12cd673229c
ChrisCore1 <ChrisCore1@users.noreply.github.com>
2026-09-01 01:20:56 -0400

    TOD-18 (feat): Interceptor (Fetch)

--- src/services/tag.service.js
+++ src/services/tag.service.js
@@ -1,45 +1,41 @@
 import { HEADERS, handleResponse, URL_TAG } from './service';
+import { fetchInterceptor } from '../utils/fetchInterceptor';
 
 export const getAll = async () => {
-    const response = await fetch(URL_TAG, {
+    const response = await fetchInterceptor(URL_TAG, {
         method: 'GET',
-        headers: HEADERS
     });
     const data = await handleResponse(response);
     return data.tags?.data || data;
 };
 
 export const create = async (tagData) => {
-    const response = await fetch(URL_TAG, {
+    const response = await fetchInterceptor(URL_TAG, {
         method: 'POST',
-        headers: HEADERS,
         body: JSON.stringify(tagData),
     });
     return handleResponse(response);
 };
 
 export const getOne = async (id) => {
-    const response = await fetch(URL_TAG + `/${id}`, {
+    const response = await fetchInterceptor(URL_TAG + `/${id}`, {
         method: 'GET',
-        headers: HEADERS
     });
     const data = await handleResponse(response);
     return data.tag || data;
 };
 
 export const update = async (id, tagData) => {
-    const response = await fetch(URL_TAG + `/${id}`, {
+    const response = await fetchInterceptor(URL_TAG + `/${id}`, {
         method: 'PUT',
-        headers: HEADERS,
         body: JSON.stringify(tagData),
     });
     return handleResponse(response);
 };
 
 export const deleteTag = async (id) => {
-    const response = await fetch(URL_TAG + `/${id}`, {
+    const response = await fetchInterceptor(URL_TAG + `/${id}`, {
         method: 'DELETE',
-        headers: HEADERS,
     });
     const data = await handleResponse(response);
     return data.tag || data;
